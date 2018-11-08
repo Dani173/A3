bf58396702801190dc56243dd7a8f20625c380b4
Dani173 <32301542+Dani173@users.noreply.github.com>
2018-11-08 18:41:29 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+
+<?php
+session_start();
+$_SESSION['host']="dgonzalez.cesnuria.com";
+       $_SESSION['userdb']="dgonzalez_dani";
+        $_SESSION['passdb']="123123";
+          $_SESSION['db']="dgonzalez_AC3_2";
+     
+     
+
+?>
+
+<html>
+    <head>
+      <link rel="stylesheet" type="text/css" href="css/css.css"> 
+        <title>TODOList</title>
+    </head>
+    <body class="bindex">
+    <h1>Bienvenido a TODOList</h1>
+     <div>
+        <a href="entrar.php">Login</a>
+   <a href="crear.php">Registrarse</a>
+   </div>
+    </body>
+</html>
